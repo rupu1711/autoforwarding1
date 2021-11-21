e4fe9dd53129837fdcb7b4c9356d474f2063a751
Rupesh Kumar <rupesh1711@gmail.com>
2021-11-21 13:33:13 +0530

    first commit

---
+++ telegram_user.py
@@ -0,0 +1,24 @@
+
+from telethon import TelegramClient, events
+
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+api_id = 19634185
+api_hash = 'b3b9dddf1fe26908bca459a96c89c3f1'
+
+
+client = TelegramClient('AutoForward', api_id, api_hash)
+
+@client.on(events.NewMessage)
+async def handler(event):
+  chat=await event.get_chat()
+  chat_id = event.chat_id
+  print("{}{}".format(chat_id,chat))
+
+  if chat_id == -1001184107063:
+      await client.send_message(-1001580693925, event.raw_text)
+  
+client.start()
+client.run_until_disconnected()
+
